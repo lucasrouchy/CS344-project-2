016076748eb3343f4b6bc74ba16c90846c597cce
Lucas <55973521+lucasrouchy@users.noreply.github.com>
2022-01-17 21:31:37 -0800

    since i didnt use the printf it didnt print anything to the terminal window so I know it compiles I just don't know if it actually read the files.

---
+++ readfile.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include <fcntl.h>
+#include<errno.h>
+#include <unistd.h>
+int main(int argc, char *argv[]){
+    char buf[2048];
+    size_t nbytes;
+    ssize_t bytes_read;
+
+    nbytes = sizeof(buf);
+
+    for (int i = 1; i < argc; i++){
+        int files = open(argv[i], O_RDWR);
+        bytes_read = read(files, buf, nbytes);
+        write(files, buf, nbytes);
+        close(files);
+      }
+}
